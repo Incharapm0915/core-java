6abd017e669b95fa2d01b82c40404ee66cbe9306
Incharapm0915 <incharapm15@gmail.com>
2026-03-05 11:15:56 +0530

    updated files

---
+++ UserProfilee.java
@@ -0,0 +1,9 @@
+class UserProfilee {
+    static int age = 90;
+    public static void main(String[] args) {  
+
+        int age = 78;
+        System.out.println("The user's age is: " + UserProfilee.age);
+        System.out.println("The user's age is: " + age);
+    }
+}   
